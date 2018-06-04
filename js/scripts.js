9607a3c889696df6d37e0df6c56d84e91529c855
Kevin Ahn and Thad Donaghue <thad@>
2018-06-04 15:05:37 -0700

    Add toggle feature for cards

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,7 @@ $(document).ready(function() {
   // Build a deck
   $("#showCards").click(function() {
     makeDeck();
-    $('#showCards').attr('disabled','disabled');
-    $("#output").show();
+    $("#output").toggle();
   });
 
   function makeDeck() {
@@ -39,35 +38,6 @@ $(document).ready(function() {
       });
     });
   };
-
-/*
-Word Order
-Create a website that lets users input a block of text and returns a list of all the unique words and how many times they appeared.
-
-For example, if the user inputs "hello world world", the page should show:
-
-hello 1
-world 2
-If you finish, and feel comfortable with forEach() loops, try to complete the above exercise but instead order the list by the number of appearances - greatest to least. For words that appear the same amount of times, order by which word appeared first.
-
-Hint (and sneak peek at next week): Try creating and playing with an object hash and its contents in the JavaScript console:
-*/
-
-// declare an object for each entry the user makes. Each word will get saved into a separate object and initialized with a value of 0. Use branching to find if word exists in an object.
-
-  // function wordMatch() {
-  //   var words = [];
-  //   var matches = 0;
-  //
-  //   for(i = 0; i < 10; i++) {
-  //     words[i] = {$("#word").val(), matches}
-  //   }
-  //
-  //   words.forEach(function() {
-  //
-  //   });
-  //
-  // }
 
   $("#findMatches").click(function(event) {
     event.preventDefault();
